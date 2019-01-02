bcf55a308ca9129eae51d4ed819dddfb5818c464
Jean Marte <jeancmarte21@gmail.com>
2019-01-01 20:45:30 -0400

    bug fix

--- src/main/java/pract/ProyectoApplication.java
+++ src/main/java/pract/ProyectoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableJpaRepositories
-public class Practica10Application {
+public class ProyectoApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(Practica10Application.class, args);
+        SpringApplication.run(ProyectoApplication.class, args);
     }
 }
